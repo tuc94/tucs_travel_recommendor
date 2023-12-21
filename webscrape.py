55947a60fecc993a8fe83a1d2716de4ba66ca99e
tuc94 <tuc.baldecchi@gmail.com>
2023-12-20 21:34:54 -0500

    added data scraping

--- webscrape.py
+++ webscrape.py
@@ -21,8 +21,30 @@ element.click()
 time.sleep(30)
 
 
-# Proceed with your scraping or further actions
-# ...
+# Locate the <ul> element inside the div with id="GeographicDemandCountry"
+ul_element = driver.find_element(By.CSS_SELECTOR, '#GeographicDemandCountry ul')
+
+# Find all <li> elements within the <ul>
+li_elements = ul_element.find_elements(By.TAG_NAME, 'li')
+
+# Initialize a list to hold the scraped data
+scraped_data = []
+
+# Loop through each <li> element
+for li in li_elements:
+    # Extract text from spans
+    name_span = li.find_element(By.CLASS_NAME, 'geographic-demand__name')
+    total_span = li.find_element(By.CLASS_NAME, 'geographic-demand__total')
+
+    name = name_span.text
+    total = total_span.text
+
+    # Store the data in a dictionary and add it to the list
+    scraped_data.append({'name': name, 'total': total})
 
 # Close the browser
 driver.quit()
+
+# Print or process the scraped data
+for data in scraped_data:
+    print(data)
